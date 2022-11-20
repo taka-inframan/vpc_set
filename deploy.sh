ab522d75c9ce696cfc90d93167fc64b632298a75
taka-inframan <into@plus-go.net>
2022-11-20 17:42:47 +0900

    繰り返し箇所を最適化

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,18 @@
 #!/bin/bash
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
+STACKS=(vpc security web)
 
-echo "Start create vpc."
-echo $DATE
-aws cloudformation deploy \
---template-file vpc.yaml \
---stack-name vpc
-AWS_CF_RETURN=$?
-if [[ $AWS_CF_RETURN -ne 0 ]]; then
-        echo "[ERROR]CloudFormation create VPC error." >&2
-        exit 1
-fi
-echo "End create vpc."
-echo $DATE
-echo "---"
-echo "Start create security."
-echo $DATE
-aws cloudformation deploy \
---template-file security.yaml \
---stack-name security
-AWS_CF_RETURN=$?
-if [[ $AWS_CF_RETURN -ne 0 ]]; then
-        echo "[ERROR]CloudFormation create Security error." >&2
-        exit 1
-fi
-echo "End create security."
-echo $DATE
-echo "---"
-echo "Start create web."
-echo $DATE
-aws cloudformation deploy \
---template-file web.yaml \
---stack-name web
-AWS_CF_RETURN=$?
-if [[ $AWS_CF_RETURN -ne 0 ]]; then
-        echo "[ERROR]CloudFormation create Web error." >&2
-        exit 1
-fi
-echo "End create Web."
-echo $DATE
-echo "---"
-echo "Deploy End."
-echo $DATE
+for stack in ${STACKS[@]}
+do
+        echo "Start create ${stack} |" `date '+%Y-%m-%d %H:%M:%S'`
+        aws cloudformation deploy \
+        --template-file ${stack}.yaml \
+        --stack-name ${stack}
+        if [[ $AWS_CF_RETURN -ne 0 ]]; then
+                echo "[ERROR]CloudFormation create ${stack} error." >&2
+                exit 1
+        fi
+        echo "End create ${stack} |" `date '+%Y-%m-%d %H:%M:%S'`
+        echo "---"
+done
+echo "Deploy End |" `date '+%Y-%m-%d %H:%M:%S'`
